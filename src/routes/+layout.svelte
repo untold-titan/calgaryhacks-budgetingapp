2e60d5ab0a14c4e514aaabfe98d746eea637957b
untold-titan <untoldtitan3k@gmail.com>
2025-02-15 16:15:02 -0700

    Implement Firebase Auth

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { initializeApp } from "@firebase/app";
+    import { setContext } from "svelte";
+  
+    // Init firebase at the root of the app so it can be passed down into the rest of the app.
+    let { data } = $props();
+  
+    const firebaseConfig = {
+      apiKey: data.firebaseKeys.apikey,
+      authDomain: data.firebaseKeys.authdomain,
+      projectId: data.firebaseKeys.projid,
+      storageBucket: data.firebaseKeys.bucket,
+      appId: data.firebaseKeys.appid,
+    };
+    
+    export const app = initializeApp(firebaseConfig);
+  </script>
+  <slot></slot>
