29b3d53f3b3a59a823dd4df5bbeda530b72233bf
EvanRosenblood <ewrosenb@ualberta.ca>
2025-02-15 23:40:42 -0700

    HalfWay

--- src/routes/app/taxes/+page.svelte
+++ src/routes/app/taxes/+page.svelte
@@ -1,26 +1,40 @@
 
-<img src="/t4.jpg" alt="T4" class="T4-img">
 
-<h1><br><br>1. Locate your T4</h1>
-<br>
-<h3>You get this from your employer either physically or digitally</h3>
-<h3>It's a list of how much you owe in taxes and where the money goes</h3>
-<h3>What is your income listed on number 14, Employment Income?</h3>
-<h3>
-<label for="Income">Enter a money value: $</label>
-<input type="number" id="Income" placeholder="e.g. 1234.56" style="width: 90px; margin-left: 10px"/>
-</h3>
+<div class="introduction">
+    <div>
+        <h1><br><br><br>Introduction</h1>
+        <h3>This is a walk through for filing your taxes</h3>
+        <h3>Use the links above to jump to certain sections of the process</h3>
+        <h3>There are a few mandatory pieces of information needed before starting such as a CRA account and a T4 form</h3>
+        <h3>You get the T4 from your employer either physically or digitally</h3>
+        <h3>It's a list of how much you owe in taxes and where the money goes</h3>
+        <h3>Use the links provided as you go through to your advantage as they are safe and very helpful</h3>
+        <br><br><br><br><br><br><br><br><br>
+    </div>
+    <div>
+        <img src="/t4.jpg" alt="T4" class="T4-img">
+    </div>
+</div>
 
-<h3>
-    - Using Canada Revenue Agency (CRA) Manditory infomration needed.  <a href="https://www.canada.ca/en/revenue-agency/services/e-services/cra-login-services.html">CRA Login</a>
-    <br>- Using a thrid party to help like   <a href="https://turbotax.intuit.ca/tax/software/online?cid=pser_en_TT_2025_Google_Shopping_pMAX_EN_google_gkw_gen_NA_cg_assetgroup=%7Bassetgroup%7D_product_id=_zen_campaignid=19581176602&gad_source=1&gclid=EAIaIQobChMI8ISEiLrHiwMVA9DCBB0vJSSeEAAYASAAEgJOyvD_BwE&gclsrc=aw.ds">TurboTax</a>
-    <br>- Or <a href= "https://www.wealthsimple.com/en-ca/tax?utm_term=wealthsimple%20tax&matchtype=e&campaign=21039355834&adgroup=159830487115&gad_source=1&gclid=CjwKCAiAk8G9BhA0EiwAOQxmfoW338AD-0zN0oyxCiSKCR5MLgnRhhbsUpP6-jhZ1-qhFckkTanBjBoCNLkQAvD_BwE">WealthSimple Tax</a>
-    <br>- Depending on where you live you have to order then ship back
-    <br> ordering is  <a href="https://apps.cra-arc.gc.ca/ebci/cjcf/fpos-scfp/pub/rdr?searchKey=2024+Package#formCatalog">Ordering T4/T1</a>
-    <br> shiping your shipping your t4 is <a href="https://www.canada.ca/en/revenue-agency/corporate/contact-information/where-mail-your-paper-t1-return.html">Sending T4/T1</a>
+<div class="infoNeeded">
+    <h1>Information Needed Before Starting</h1>
+    <h3>
+        <br>Canada Revenue Agency (CRA) account is needed.  <a href="https://www.canada.ca/en/revenue-agency/services/e-services/cra-login-services.html" target="blank">CRA Login</a>
+    </h3>
+    <h3>T4 form ensuring all personal information is correct. If not, call the CRA at 1-(800)-959-8281</h3>
+    <h3>If you have investments, a T5 is mandatory for tax returns. <br>You can acecss them on the banks app under Documents or by calling them</h3>
+    <h3>
+        If you have any <a href="https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses.html" target="blank">tax deductions</a>, have the receipts or proof of purchase.
+    </h3>
     
-    <br> - T5 is when you make investments, banks/investment provider
-</h3>
+
+        <br>Using a thrid party to help like   <a href="https://turbotax.intuit.ca/tax/software/online?cid=pser_en_TT_2025_Google_Shopping_pMAX_EN_google_gkw_gen_NA_cg_assetgroup=%7Bassetgroup%7D_product_id=_zen_campaignid=19581176602&gad_source=1&gclid=EAIaIQobChMI8ISEiLrHiwMVA9DCBB0vJSSeEAAYASAAEgJOyvD_BwE&gclsrc=aw.ds" target="blank">TurboTax</a> Or <a href= "https://www.wealthsimple.com/en-ca/tax?utm_term=wealthsimple%20tax&matchtype=e&campaign=21039355834&adgroup=159830487115&gad_source=1&gclid=CjwKCAiAk8G9BhA0EiwAOQxmfoW338AD-0zN0oyxCiSKCR5MLgnRhhbsUpP6-jhZ1-qhFckkTanBjBoCNLkQAvD_BwE" target="blank">WealthSimple Tax</a>
+        <br>Depending on where you live you have to order then ship back
+        <br> ordering is  <a href="https://apps.cra-arc.gc.ca/ebci/cjcf/fpos-scfp/pub/rdr?searchKey=2024+Package#formCatalog" target="blank">Ordering T4/T1</a>
+        <br> shiping your shipping your t4 is <a href="https://www.canada.ca/en/revenue-agency/corporate/contact-information/where-mail-your-paper-t1-return.html" target="blank">Sending T4/T1</a>
+        <br> T5 is when you make investments, banks/investment provider
+</div>
+<br><br><br><br><br><br><br>
 <script>    
     /** @type {{ data: import('./$types').PageData }} */
     let { data } = $props();
@@ -28,15 +42,18 @@
 </script>
 
 <style>
-    .content {
+    .introduction {
         display: flex;
+        text-align:center;
+    }
+
+    .infoNeeded {
         text-align:center;
     }
    
     .T4-img {
       width: 59.5vw;
       height: auto;         
-      position: fixed;      
       bottom: 35px;
       right: 10px;
       object-fit: cover;  
