d63008f4e8637f55e2bd7c2382d4c3cb34af4c43
untold-titan <untoldtitan3k@gmail.com>
2025-02-15 22:22:58 -0700

    create budgeting page

---
+++ src/components/Gauge.svelte
@@ -0,0 +1,44 @@
+<div class="container">
+    <div class="under">
+        <img src="/corclequarter.png">
+    </div>
+    <div class="gauge">
+    </div>
+    <div class="text">
+        Text
+    </div>
+</div>
+
+<style>
+    .container{
+        width: 250px;
+        height: 350px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .gauge{
+        position: relative;
+        background-color: #141416;
+        width: 150px;
+        height: 150px;
+        rotate: 45deg;
+        top: 150px;
+        left: 50px;
+    }
+    .under{
+        position: absolute;
+        display: block;
+        width: 250px;
+        height: 250px;
+        rotate: -45deg;
+        z-index: auto;
+    }
+    img{
+        width: 250px;
+        height: 250px;
+    }
+    @keyframes coolanimationbecauseimjustthatgood{
+        
+    }
+
+</style>
---
+++ src/routes/app/budgeting/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+  import Loading from "../../../components/Loading.svelte";
+  import Gauge from "../../../components/Gauge.svelte";
+
+</script>
+
+<Gauge/>
+<Loading />
+
+<style>
+  .chart {
+    width: 500px;
+    height: 500px;
+  }
+</style>
