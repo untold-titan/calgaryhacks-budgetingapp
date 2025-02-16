d29c31b2868c7e7a9a1a5a9692e6ccffed201412
dep-dep <harderick.d@gmail.com>
2025-02-16 08:21:51 -0700

    up1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,6 +4,9 @@
   import { user } from "$lib/stores";
   import { getAuth } from "@firebase/auth";
   import { signInWithEmailAndPassword } from "firebase/auth/cordova";
+  import "../../styles/styles.css";
+    import Navigation from "../../components/Navigation.svelte";
+
   const auth = getAuth();
 
   let email = $state();
@@ -21,9 +24,33 @@
   }
 </script>
 
-<p>Login Page</p>
-<label>Email</label>
-<input bind:value={email} />
-<label>Password</label>
-<input bind:value={password} />
-<button onclick={LoginUser}>Login</button>
+<div class="header">
+  <h1>SoloSense</h1>
+  <Navigation/>
+</div>
+
+<div class="signup">
+  <h1>Login</h1>
+  <!-- <label>Email</label> -->
+  <input bind:value={email} placeholder="Email"/><br>
+  <!-- <label>Password</label><br> -->
+  <input bind:value={password} placeholder="Password"/><br>
+  <button class="button" onclick={LoginUser}>Login</button>
+</div>
+
+<style>
+  .header {
+    padding-left: 20px;
+    display: flex;
+    align-items: center;
+    height: 70px;
+    color: #ffffff;
+    background-color: #202124;
+  }
+  .header h1 {
+    margin-right: auto;
+  }
+  .login {
+    margin-top: 500px;
+  }
+</style>
