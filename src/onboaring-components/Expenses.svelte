980dc6c0ae5b5a57feb01b0954478be98d1e7e77
Bilal Ahmed <bilalahmed1905@gmail.com>
2025-02-15 22:35:45 -0700

    added the date of the expense as the 3rd input

--- src/onboaring-components/Expenses.svelte
+++ src/onboaring-components/Expenses.svelte
@@ -2,7 +2,7 @@
     let expenses = [{ name: "", amount: "" }]; // Store expenses in an array
 
     function addExpense() {
-        expenses = [...expenses, { name: "", amount: "" }]; // Add a new empty expense object
+        expenses = [...expenses, { name: "", amount: "", date: ""}]; // Add a new empty expense object
     }
 </script>
 
@@ -11,17 +11,19 @@
         New Expense
     </button>
 </div>
-
+<br>
 <div class="flex">
-    <label style="width: 50%;">Expense Name</label>
-    <label style="width: 50%">Expense Amount</label>
+    <label style="width: 33.3%;">Expense Name</label>
+    <label style="width: 33.3%">Expense Amount</label>
+    <label style="width: 33.3%">Expense Date</label>
 </div>
 
 <div id="expenses">
     {#each expenses as expense, index}
         <div class="flex">
             <input class="expenseName" bind:value={expenses[index].name} placeholder="Expense Name">
-            <input type="text" bind:value={expenses[index].amount} placeholder="Expense Amount">
+            <input style="width: 33.3%" type="text" bind:value={expenses[index].amount} placeholder="Expense Amount">
+            <input style="width: 33.3%" type="date" bind:value={expenses[index].date}>
         </div>
     {/each}
 </div>
