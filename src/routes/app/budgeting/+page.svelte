b80189bc654bc0ba2f1a108b80c354456092a37b
untold-titan <untoldtitan3k@gmail.com>
2025-02-16 01:58:59 -0700

    UI Complete, time to connect things

--- src/routes/app/budgeting/+page.svelte
+++ src/routes/app/budgeting/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import Loading from "../../../components/Loading.svelte";
   import Gauge from "../../../components/Gauge.svelte";
+  import Timeline from "../../../components/Timeline.svelte";
 
   let loading = $state(false);
 
@@ -13,19 +14,21 @@
 {#if loading}
   <Loading />
 {:else}
-  <div>
+  <div class="cont">
     <div class="gauges">
         <Gauge topText="$20500" bottomText="Annual Income" percent=50 remaining="20500 out of 25000"/>
         <Gauge topText="$5250" bottomText="Monthly Expenses" percent=25 remaining=""/>
     </div>
-    <div class="timeline">
-
-    </div>
+    <Timeline/>
   </div>
 {/if}
 
 <style>
+    .cont{
+        display: flex;
+    }
     .gauges{
+        margin-top: 10%;
         display: flex;
         width: 70%;
     }
