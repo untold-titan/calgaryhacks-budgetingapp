1ac28c2c0c92ca3e0651fae9e5fac6d772265315
Bilal Ahmed <bilalahmed1905@gmail.com>
2025-02-16 10:07:40 -0700

    fixed nav

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,6 +1,6 @@
 <nav>
   <ul style="display: flex;">
-    <li style="margin-right: 120px;"><a href="/signup" style="color: white;text-decoration: underline;">Sign Up</a></li>
-    <li><a href="/login" style="color: white;text-decoration: underline;">Login</a></li>
+    <li style="margin-right: 30px;"><a href="/signup" style="color: white;">Sign Up</a></li>
+    <li><a href="/login" style="color: white;margin-right: 20px;">Login</a></li>
   </ul>
 </nav>
