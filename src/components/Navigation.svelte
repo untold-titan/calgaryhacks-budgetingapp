8e9dfc0ffe8def5217b2ad7c40a91324118e8772
untold-titan <untoldtitan3k@gmail.com>
2025-02-16 07:57:18 -0700

    bruh

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,9 +1,7 @@
 <nav>
   <ul>
-    <li><a href="/">Home</a></li>
     <li><a href="/signup">Sign Up</a></li>
     <li><a href="/login">Login</a></li>
-    <li><a href="/calendar">Calendar</a></li>
   </ul>
 </nav>
 
